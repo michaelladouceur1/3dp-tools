a90571dc299448e9258c671b74399218de02c7a1
Michael LaDouceur <michaelladouceur1@gmail.com>
2022-09-07 00:29:48 -0500

    updated comments for plugins.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,4 +2,8 @@ import * as config from "./config/config";
 
 export async function start() {
 	config.setup();
+
+	// load plugins
+
+	// load environment
 }
--- src/core/plugins/index.ts
+++ src/core/plugins/index.ts
@@ -2,6 +2,12 @@ import { readdir } from "fs/promises";
 import * as path from "path";
 import { iPlugin } from "../../../types/plugins";
 
+/* 
+parse file for tracking downloaded and active plugins
+check if plugins are most recent versions
+check if auto update is turned on and update if so
+load plugins and return
+*/
 export default async function loader(pluginsPath: string, plugins: iPlugin[]) {
 	try {
 		const pluginDirs = await readdir(pluginsPath);
